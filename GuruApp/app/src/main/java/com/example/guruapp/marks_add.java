525200843678efcb660b10dc741ebe501ea762b0
gnanod <1995gnanod@gmail.com>
2019-07-30 13:50:48 +0530

    no message

---
+++ GuruApp/app/src/main/java/com/example/guruapp/marks_add.java
@@ -0,0 +1,49 @@
+package com.example.guruapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class marks_add extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ///////////////////////////////////////////----Spinner ----/////////////////////////////////////////////////
+
+
+    }
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        View v = inflater.inflate(R.layout.activity_marks_add,container,false);
+
+        String [] values =
+                {"Galle","Mathara","Hambanthota",};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        return v;
+
+
+    }
+
+}
